a3eaafedcc34c712c5f6460960d094803a3f5b46
Rodriguez1973 <j062667@gmail.com>
2022-12-31 07:13:44 +0100

    Fin de la depuración.

--- js/GoogleMaps.js
+++ js/GoogleMaps.js
@@ -116,8 +116,10 @@ function añadirMarcador(geolocalizacion) {
           marcadores[marcadores.length - 1].position,
         ) * 100,
       ) / 100
+  //Si es la última muestra.
   } else if (ultimaMuestra) {
-    if (marcadores.length > 1) {
+    if (marcadores.length > 0) {
+      geolocalizacion = marcadores[marcadores.length - 1].position
       distanciaEntrePuntos =
         Math.round(
           calcularDistancia2Puntos(
@@ -126,6 +128,7 @@ function añadirMarcador(geolocalizacion) {
           ) * 100,
         ) / 100
     }
+    marcadores.pop()  //Elimina el último elemento del array.
   }
 
   //Si es la primera o última muestra, o cualquier otra posición a una distancia con el punto anterior igual o superior la distancia mínima.
